9e60914e91dd51e65a48411f8c14147b5af25de6
nguyensang <nguyensang@vnvn.net>
2023-10-05 19:21:16 +0700

    fix code

--- src/modules/job/job.service.ts
+++ src/modules/job/job.service.ts
@@ -80,6 +80,7 @@ export class JobService {
           state: true,
           county: true,
           status: true,
+          statusData: true,
           duration: true,
           createdAt: true,
         },
